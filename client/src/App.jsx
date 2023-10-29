7a2222fb0ca6ddf47a3175e36b6eccb24c16ea30
MarvelousNtp <natthapong-t@hotmail.com>
2023-10-29 19:44:40 +0700

    add AdminNavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
+import AdminNavbar from "./components/AdminNavbar";
 //import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
     </div>
   );
 }
