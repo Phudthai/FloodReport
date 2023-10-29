8fca1fa3e294461d77bfb095f655065d374f98cc
MarvelousNtp <natthapong-t@hotmail.com>
2023-10-29 17:58:04 +0700

    nav-bar

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css";
+import logoImg from "../images/logo-icon-s-blue.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="nav-buttons">
+        <button className="btn custom-button1" href="#" >แจ้งน้ำท่วม</button>
+        <button className="btn custom-button2" href="#">ตรวจสอบพื้นที่ของคุณ</button>
+      </div>
+      <a className="navbar-brand" href="#">
+        <div className="Logo">
+          <img src={logoImg} className="logo-image"/>
+        </div>
+      </a>
+      <div className="navbar-divider"></div>
+    </nav>
+  );
+}
